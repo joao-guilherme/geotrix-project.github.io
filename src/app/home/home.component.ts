399f3b103a74e4bd9eb73229df1ac608bd81f075
geotrix-project <geotrix.project@gmail.com>
2019-08-19 10:07:03 -0300

    feat: inclusão do banner na tela inicial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChildren } from '@angular/core';
+import EmblaCarousel from 'embla-carousel';
 
 @Component({
   selector: 'geotrix-home',
@@ -7,8 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  deferredPrompt;
+
   constructor() { }
 
   ngOnInit() {
+    // const carousel: Element = document.querySelector('.embla');
+
+    // const nextBtn: Element = carousel.querySelector('.embla__button--next');
+    // const previousBtn: Element = carousel.querySelector('.embla__button--prev');
+
+    // const embla = EmblaCarousel(carousel.querySelector('.embla__viewport'), {
+    //   loop: true,
+    //   startIndex: 1,
+    //   align: 'center',
+    //   draggable: true,
+    //   dragFree: false,
+    //   slidesToScroll: 1
+    // });
+
+    // nextBtn.addEventListener('click', embla.scrollNext, false);
+    // previousBtn.addEventListener('click', embla.scrollPrev, false);
+  }
+
+  onInstallClick() {
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+    });
   }
 }
