ee0dab9f4646f1f2e7b5dfd339e6584e2497e4f1
João Guilherme M. Guimarães <joaog95@live.com>
2019-08-20 16:51:55 -0300

    feat: adicionado botão para instalação do PWA na tela home
    lógica responsável pelo evento beforeinstallprompt movido para o arquivo
    home.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Routing
 import { AppRoutingModule } from './app-routing.module';
 
 // Components
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -18,6 +19,7 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeaderComponent,
     FooterComponent,
     PageNotFoundComponent
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, HostListener, ViewChildren } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+
 import EmblaCarousel from 'embla-carousel';
 
 @Component({
@@ -8,11 +10,27 @@ import EmblaCarousel from 'embla-carousel';
 })
 export class HomeComponent implements OnInit {
 
-  deferredPrompt;
+  private deferredPrompt: any;
+  public showButton = false;
 
-  constructor() { }
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(evt: any) {
+    console.log(evt);
+    evt.preventDefault();
+
+    this.showButton = true;
+    this.deferredPrompt = evt;
+  }
+
+  // window.addEventListener('appinstalled', (evt) => {
+  //   console.log('Instalação realizada com sucesso.');
+  // });
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    // *ngIf="!router.url.includes('home')"
+
     // const carousel: Element = document.querySelector('.embla');
 
     // const nextBtn: Element = carousel.querySelector('.embla__button--next');
@@ -31,10 +49,13 @@ export class HomeComponent implements OnInit {
     // previousBtn.addEventListener('click', embla.scrollPrev, false);
   }
 
-  onInstallClick() {
+  addToHomeScreen() {
     this.deferredPrompt.prompt();
-    this.deferredPrompt.userChoice.then((choiceResult) => {
+
+    this.deferredPrompt.userChoice.then((choiceResult: any) => {
       if (choiceResult.outcome === 'accepted') {
+        this.showButton = false;
+        this.deferredPrompt = null;
         console.log('User accepted the A2HS prompt');
       } else {
         console.log('User dismissed the A2HS prompt');
