ec156add573b7c819e5769936824f37f0c529392
João Guilherme M. Guimarães <joaog95@live.com>
2019-09-18 20:06:42 -0300

    fix: habilitando Ivy e atualizando pacotes

--- src/app/matrix/determinant/determinant.component.ts
+++ src/app/matrix/determinant/determinant.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DeterminantComponent implements OnInit {
 
   public showSolution = false;
+  public matrix = [['2', '3', '4'], ['5', '10', '11'], ['3', '1', '7']];
   public sarrusResolution: string[] = ['\\sum_{i=1}^nx_i', '\\sum_{i=2}^nx_i', '\\sum_{i=3}^nx_i',
   '\\begin{vmatrix}1 & 2 & 3 \\cr 4 & 5 & 6 \\cr 7 & 8 & 9\\end{vmatrix}'];
 
